3f0a808b062201cf378f47903bf56eb3febdf323
Andrew Molchanov <mr.catmanrus@gmail.com>
2021-01-29 21:23:02 +0300

    Мелкие правки

--- pages/wiki/0.1.0/getting-started/index.vue
+++ pages/wiki/0.1.0/getting-started/index.vue
@@ -2,7 +2,7 @@
 import Vue from 'vue'
 export default Vue.extend({
   layout: 'wiki',
-  mounted() {
+  created() {
     this.$router.replace('/wiki/0.1.0/getting-started/introduction')
   },
   head: {
